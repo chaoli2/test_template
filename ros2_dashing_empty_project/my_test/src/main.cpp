bc628b133e5139ab762debc9acb4b9f223efb178
Chao Li <chao1.li@intel.com>
2019-08-01 15:46:02 +0800

    fix buid issue for message filter
    Signed-off-by: Chao Li <chao1.li@intel.com>

--- ros2_dashing_empty_project/my_test/src/main.cpp
+++ ros2_dashing_empty_project/my_test/src/main.cpp
@@ -1,9 +1,13 @@
 #include <rclcpp/rclcpp.hpp>
 #include <message_filters/subscriber.h>
-#include <message_filters/synchronizer.h>
+#include <message_filters/time_synchronizer.h>
+#include <sensor_msgs/msg/image.hpp>
+#include <std_msgs/msg/float32.hpp>
 
 #include <iostream>
 #include <memory>
+
+using namespace std::placeholders;
 
 class MessageFilterTest : public rclcpp::Node
 {
@@ -14,19 +18,21 @@ public:
     cam_sub_ = std::make_unique<camSub>(node, "/camera/color/image_raw");
     obj_sub_ = std::make_unique<objSub>(node, "/movidius_ncs_stream/detected_objects");
     sync_sub_ = std::make_unique<sync>(*cam_sub_, *obj_sub_, 10);
-    sync_sub_->registerCallback(&DetectionShow::showImage, this);
+    sync_sub_->registerCallback(std::bind(&MessageFilterTest::callback, this));
   };
   ~MessageFilterTest() = default;
 
 private:
   using camSub = message_filters::Subscriber<sensor_msgs::msg::Image>;
-  using objSub = message_filters::Subscriber<std::msg::Float32>;
+  using objSub = message_filters::Subscriber<std_msgs::msg::Float32>;
   using sync =
-    message_filters::TimeSynchronizer<sensor_msgs::msg::Image, object_msgs::msg::ObjectsInBoxes>;
+    message_filters::TimeSynchronizer<sensor_msgs::msg::Image, std_msgs::msg::Float32>;
 
   std::unique_ptr<camSub> cam_sub_;
   std::unique_ptr<objSub> obj_sub_;
   std::unique_ptr<sync> sync_sub_;
+
+  void callback() {};
 };
 
 
