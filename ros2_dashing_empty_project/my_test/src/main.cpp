a56aa4c93fcf5be62bbcbca9ebfe258d59f75fd5
Chao Li <chao1.li@intel.com>
2019-08-09 11:27:39 +0800

    add paramters for callback
    Signed-off-by: Chao Li <chao1.li@intel.com>

--- ros2_dashing_empty_project/my_test/src/main.cpp
+++ ros2_dashing_empty_project/my_test/src/main.cpp
@@ -1,13 +1,14 @@
 #include <rclcpp/rclcpp.hpp>
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
 #include <sensor_msgs/msg/image.hpp>
 #include <std_msgs/msg/float32.hpp>
 
 #include <iostream>
 #include <memory>
 
-using namespace std::placeholders;
+//using namespace std::placeholders;
 
 class MessageFilterTest : public rclcpp::Node
 {
@@ -15,24 +16,26 @@ public:
   MessageFilterTest() : Node("test") 
   {
     rclcpp::Node::SharedPtr node = std::shared_ptr<rclcpp::Node>(this);
+
     cam_sub_ = std::make_unique<camSub>(node, "/camera/color/image_raw");
     obj_sub_ = std::make_unique<objSub>(node, "/movidius_ncs_stream/detected_objects");
     sync_sub_ = std::make_unique<sync>(*cam_sub_, *obj_sub_, 10);
-    sync_sub_->registerCallback(std::bind(&MessageFilterTest::callback, this));
+    sync_sub_->registerCallback(std::bind(&MessageFilterTest::callback, this, std::placeholders::_1, std::placeholders::_2));
   };
   ~MessageFilterTest() = default;
 
 private:
   using camSub = message_filters::Subscriber<sensor_msgs::msg::Image>;
-  using objSub = message_filters::Subscriber<std_msgs::msg::Float32>;
+  using objSub = message_filters::Subscriber<sensor_msgs::msg::Image>;
+
   using sync =
-    message_filters::TimeSynchronizer<sensor_msgs::msg::Image, std_msgs::msg::Float32>;
+     message_filters::TimeSynchronizer<sensor_msgs::msg::Image, sensor_msgs::msg::Image>;
 
   std::unique_ptr<camSub> cam_sub_;
   std::unique_ptr<objSub> obj_sub_;
   std::unique_ptr<sync> sync_sub_;
 
-  void callback() {};
+  void callback(const sensor_msgs::msg::Image::ConstSharedPtr msg1, const sensor_msgs::msg::Image::ConstSharedPtr msg2) {};
 };
 
 
